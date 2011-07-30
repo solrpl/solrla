2ee0b70fd948d77979f263943b3a3718505d63a8
Rafał Kuć <rafal@kuciowie.pl>
2011-07-30 13:47:27 +0200

    Collector work

--- src/main/java/pl/solr/solrla/worker/SingleThreadedWorker.java
+++ src/main/java/pl/solr/solrla/worker/SingleThreadedWorker.java
@@ -1,8 +1,11 @@
 package pl.solr.solrla.worker;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import pl.solr.solrla.collector.Collector;
+import pl.solr.solrla.input.InputHandler;
+import pl.solr.solrla.output.OutputHandler;
 
 /** 
  * Worker. 
@@ -14,5 +17,33 @@ public class SingleThreadedWorker {
     /** Collectors. */
     protected List<Collector> collectors;
     
+    /** Output handler. */
+    private OutputHandler outputHandler;
+
+    /** Input handler. */
+    private InputHandler inputHandler;
+
     
+    /** 
+     * Add a new collector. 
+     * @param collector collector
+     */
+    public final void addCollector(Collector collector) {
+        if (collectors == null) {
+            collectors = new ArrayList<Collector>();
+        }
+        collectors.add(collector);
+    }
+    
+    public final void setCollectors(List<Collector> collectors) {
+        this.collectors = collectors;
+    }
+
+    public final void setOutputHandler(OutputHandler outputHandler) {
+        this.outputHandler = outputHandler;
+    }
+
+    public final void setInputHandler(InputHandler inputHandler) {
+        this.inputHandler = inputHandler;
+    }
 }
