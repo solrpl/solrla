bdc96063ad374ac846af06c4fb4277985c1644c8
Marek Rogoziński <m.rogozinski@solr.pl>
2011-07-30 19:16:05 +0200

    new empty test

---
+++ src/test/java/pl/solr/solrla/parser/StandardParserTest.java
@@ -0,0 +1,22 @@
+package pl.solr.solrla.parser;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import pl.solr.solrla.analyzer.parser.LogLine;
+import pl.solr.solrla.input.InputHandler;
+import static org.mockito.Mockito.*;
+
+public class StandardParserTest {
+	private StandardParser parser = new StandardParser();
+
+	@Test
+	public void first() {
+		InputHandler m = mock(InputHandler.class);
+		LogLine line = parser.readLine(m);
+		//TODO
+		//assertNotNull(line);
+	}
+
+}
